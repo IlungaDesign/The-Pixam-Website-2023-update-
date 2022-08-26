7797a9bead2275b66537b65114b7f40d50184a4c
IlungaDesign <michael.ict.info@gmail.com>
2022-08-26 13:12:26 +0200

    version mike test 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HomePage from "./Pages/Home";
+import OverOns from "./Pages/OverOns";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import MyPixam from "./Pages/MyPixam";
+import FotoDetails from "./Pages/FotoDetails";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<HomePage/>}>
+                </Route>
+                <Route path="/foto-details" element={<FotoDetails/>}>
+                </Route>
+                <Route path="/over-ons" element={<OverOns/>}>
+                </Route>
+                <Route path="/contact" element={<Contact/>}>
+                </Route>
+                <Route path="/login" element={<Login/>}>
+                </Route>
+                <Route path="/my-pixam" element={<MyPixam/>}>
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
+
